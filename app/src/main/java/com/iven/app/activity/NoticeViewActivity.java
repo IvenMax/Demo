b996ab9c2a1444dbe75b1f5d66a41b3d027f43ed
IvenMax <yanfengqiu06@163.com>
2017-01-16 17:48:58 +0800

    新建仿京东首页快报的Activity(待做 自定义view)

---
+++ app/src/main/java/com/iven/app/activity/NoticeViewActivity.java
@@ -0,0 +1,15 @@
+package com.iven.app.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.iven.app.R;
+
+public class NoticeViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice_view);
+    }
+}
