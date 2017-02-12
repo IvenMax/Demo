bba9ccca4b004ea20dac56f5cd90c15bdf621726
IvenMax <791689609@qq.com>
2017-02-12 21:21:40 +0800

    ...

---
+++ app/src/main/java/com/iven/app/view/CountDownAdView.java
@@ -0,0 +1,25 @@
+package com.iven.app.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author Iven
+ * @date 2017/2/12 21:07
+ * 广告倒计时View
+ */
+
+public class CountDownAdView extends View{
+    public CountDownAdView(Context context) {
+        super(context);
+    }
+
+    public CountDownAdView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CountDownAdView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
