531a0a2b84b5ee302f3f6d0b0dd4792bab326ace
IvenMax <yanfengqiu06@163.com>
2017-02-21 11:10:39 +0800

    pull_layout优化

--- app/src/main/java/com/iven/app/activity/PullToRefreshActivity.java
+++ app/src/main/java/com/iven/app/activity/PullToRefreshActivity.java
@@ -6,7 +6,9 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
+import com.google.gson.Gson;
 import com.iven.app.R;
 import com.iven.app.adapter.CommonAdapter;
 import com.iven.app.adapter.ViewHolder;
@@ -16,6 +18,7 @@ import com.lzy.okgo.OkGo;
 import com.lzy.okgo.callback.StringCallback;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Response;
@@ -25,12 +28,13 @@ public class PullToRefreshActivity extends AppCompatActivity {
 
     private ListView test_pull_listview;
     private ArrayAdapter<String> adapter;
-    private ArrayList<ZhiHuHistoryBean> datas;
+    private ArrayList<ZhiHuHistoryBean.StoriesBean> datas;
     //刷新相关
     private PullToRefreshLayout mPullToRefreshView;
     private ImageView mImageView;
     private String startDate = "20141101";
     private CommonAdapter mAdapter;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,15 +46,16 @@ public class PullToRefreshActivity extends AppCompatActivity {
     }
 
     private void init() {
+        datas = new ArrayList<>();
         test_pull_listview = (ListView) findViewById(R.id.test_pull_listview);
-        test_pull_listview.setAdapter(mAdapter = new CommonAdapter<ZhiHuHistoryBean>(getApplicationContext(), datas, R.layout.layout_item_pull_to_refresh) {
+        test_pull_listview.setAdapter(mAdapter = new CommonAdapter<ZhiHuHistoryBean.StoriesBean>(getApplicationContext(), datas, R.layout.layout_item_pull_to_refresh) {
             @Override
-            public void convert(ViewHolder helper, ZhiHuHistoryBean item) {
-                //数据绑定
-
+            public void convert(ViewHolder viewHolder, ZhiHuHistoryBean.StoriesBean item) {
+                //数据绑定iv_pull_to_refresh   tv_pull_to_refresh
+                viewHolder.setText(R.id.tv_pull_to_refresh, item.getTitle());
             }
         });
-
+        http_net(startDate);
     }
 
     private void initPullToRefreshLayout() {
@@ -63,7 +68,9 @@ public class PullToRefreshActivity extends AppCompatActivity {
             @Override
             public void onRefresh() {
                 mPullToRefreshView.setRefreshing(false);
-                http_net();
+                int i = Integer.parseInt(startDate);
+                int i1 = i + 1;
+                http_net(i1 + "");
             }
 
             @Override
@@ -81,13 +88,17 @@ public class PullToRefreshActivity extends AppCompatActivity {
     /**
      * 网络请求---此处使用的知乎日报的历史消息来测试，测试而已 (/ □ \)
      */
-    private void http_net() {
+    private void http_net(String startDate) {
         //http://news-at.zhihu.com/api/4/news/before/20141101
         String url = "http://news-at.zhihu.com/api/4/news/before/" + startDate;
         OkGo.get(url).execute(new StringCallback() {
             @Override
             public void onSuccess(String s, Call call, Response response) {
-
+                Gson gson = new Gson();
+                ZhiHuHistoryBean storiesBean = gson.fromJson(s, ZhiHuHistoryBean.class);
+                List<ZhiHuHistoryBean.StoriesBean> stories = storiesBean.getStories();
+                datas.addAll(stories);
+                mAdapter.notifyDataSetChanged();
             }
         });
 
@@ -96,7 +107,7 @@ public class PullToRefreshActivity extends AppCompatActivity {
 
     public View createRefreshView() {
         View headerView = mPullToRefreshView.setRefreshView(R.layout.layout_head);
-        //		progressBar = (ProgressBar) headerView.findViewById(R.id.pb_view);
+        progressBar = (ProgressBar) headerView.findViewById(R.id.pb_view);
         mImageView = (ImageView) headerView.findViewById(R.id.text_view);
         mImageView.setImageResource(R.mipmap.drawable_text);
         //		imageView = (ImageView) headerView.findViewById(R.id.image_view);
