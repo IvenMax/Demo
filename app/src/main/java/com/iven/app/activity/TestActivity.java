9558394e6b57b55435b16c82ab14731bb25ff84b
IvenMax <yanfengqiu06@163.com>
2017-03-01 13:37:11 +0800

    添加okgo的回调

--- app/src/main/java/com/iven/app/activity/TestActivity.java
+++ app/src/main/java/com/iven/app/activity/TestActivity.java
@@ -8,11 +8,18 @@ import android.view.View;
 import com.iven.app.R;
 import com.iven.app.adapter.MyRecyclerAdapter;
 import com.iven.app.bean.ColumnBean;
+import com.iven.app.bean.TestBean;
+import com.iven.app.callback.JsonCallback;
 import com.iven.app.view.BouncingMenu;
 import com.iven.app.view.FloatView;
+import com.lzy.okgo.OkGo;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Response;
 
 public class TestActivity extends AppCompatActivity {
     private static final String TAG = "zpy_TestActivity";
@@ -31,7 +38,7 @@ public class TestActivity extends AppCompatActivity {
         }
         MyRecyclerAdapter adapter = new MyRecyclerAdapter(datas);
         mBouncingMenu = BouncingMenu.makeMenu(findViewById(R.id.ll_act_test),R.layout.layout_bounch_sv,adapter);
-        mBouncingMenu.show();
+//        mBouncingMenu.show();
     }
 
     public ArrayList<ColumnBean> getData() {
@@ -59,11 +66,30 @@ public class TestActivity extends AppCompatActivity {
 
     public void loadFloatViewDate(View view) {
 //        float_view.setData(getData());
+        Log.e(TAG, "loadFloatViewDate: 68" + "行 = " );
+        OkGo.get("https://api.douban.com/v2/movie/top250?start=1&count=20")
+                .execute(new JsonCallback<TestBean>() {
+                    @Override
+                    public void onSuccess(TestBean testBean, Call call, Response response) {
+                        int count = testBean.getCount();
+                        Log.e(TAG, "onSuccess: 75" + "行 = count = " +count);
+                        Log.e(TAG, "onSuccess: 72" + "行 = " +"-----"+testBean.getTitle());
+                        List<TestBean.SubjectsBean> subjects = testBean.getSubjects();
+                        int size = subjects.size();
+                        Log.e(TAG, "onSuccess: 77" + "行 = size = " +size);
 
-        mBouncingMenu.show();
+                    }
+
+                    @Override
+                    public void onError(Call call, Response response, Exception e) {
+                        super.onError(call, response, e);
+                        Log.e(TAG, "onError: 79" + "行 = " );
+                    }
+                });
+//        mBouncingMenu.show();
     }
 
     public void stopDj(View view) {
-        mBouncingMenu.dismiss();
+//        mBouncingMenu.dismiss();
     }
 }
